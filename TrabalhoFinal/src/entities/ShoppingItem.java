19234c98879d346d444f2511e3b0f07863287d29
DURAND <junior_durand@outlook.com>
2019-06-02 10:42:04 -0300

    Criando classe ShoppingItem

---
+++ TrabalhoFinal/src/entities/ShoppingItem.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entities;
+
+/**
+ *
+ * @author durand
+ */
+public class ShoppingItem {
+    public int id;
+    public int quant;
+
+    public ShoppingItem(Product p, int quant) {
+        this.id = p.id;
+        this.quant = quant;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingItem{" 
+                + "id = " + id 
+                + ", quantidade = " + quant 
+                + '}';
+    }
+    
+    
+}
