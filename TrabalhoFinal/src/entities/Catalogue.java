958376df97320c69c5ef05463703498b6646c041
Durand <40248223+JuniorDurand@users.noreply.github.com>
2019-06-02 09:48:01 -0300

    Add files via upload

---
+++ TrabalhoFinal/src/entities/Catalogue.java
@@ -0,0 +1,53 @@
+package entities;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+
+package entities;
+
+/**
+ *
+ * @author durand
+ */
+public class Catalogue {
+    List <Product> produtos = new ArrayList<>();
+    
+    public void inserirItem(Product p){
+        produtos.add(p);
+    }
+    
+    public Product procurarItem(String nome){
+        for(Product p : produtos){
+            if(p.getNome().equals(nome)){
+                return p;
+            }
+        }
+        return null;
+    }
+    public Product procurarItem(int id){
+        for(Product p : produtos){
+            if(p.getId() == id){
+                return p;
+            }
+        }
+        return null;
+    }
+    
+    public String mostraItens(){
+        String itens = "";
+        for(Product p : produtos){
+            itens += p.toString();
+            itens += "\n";
+        }
+        return itens;
+    }
+    
+    
+}
---
+++ TrabalhoFinal/src/entities/Product.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entities;
+
+/**
+ *
+ * @author durand
+ */
+public class Product {
+    public int id;
+    public String nome;
+    public double valor;
+    public int quantEmEstok;
+
+    public Product(int id, String nome, double valor, int quantEmEstok) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+        this.quantEmEstok = quantEmEstok;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getQuantEmEstok() {
+        return quantEmEstok;
+    }
+
+    public void setQuantEmEstok(int quantEmEstok) {
+        this.quantEmEstok = quantEmEstok;
+    }
+    
+    public boolean comprar(int quant){
+        if(quant <= this.quantEmEstok){
+            this.quantEmEstok -= quant;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" 
+                + "id = " + id 
+                + ", nome = " + nome 
+                + ", valor = " + valor 
+                + ", quantidade em estoque = " + quantEmEstok 
+                + '}';
+    }
+    
+    
+    
+}
