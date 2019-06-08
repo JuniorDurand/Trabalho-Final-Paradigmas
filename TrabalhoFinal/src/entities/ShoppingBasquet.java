0e1fcdfa30614fa153da8efeec0bab59ec5bc3ee
Fernando Henrique <fernandodojo@gmail.com>
2019-06-08 20:17:25 -0300

    Auto Format

--- TrabalhoFinal/src/entities/ShoppingBasquet.java
+++ TrabalhoFinal/src/entities/ShoppingBasquet.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package entities;
 
 import java.util.ArrayList;
@@ -14,70 +13,66 @@ import java.util.List;
  * @author durand
  */
 public class ShoppingBasquet {
-    List <ShoppingItem> cestaDeCompras = new ArrayList<>();
-    
-    //Chamar metodo compra do Product na classe acima.
 
-    public void AddShopItens(Product p, int quant){
+    List<ShoppingItem> cestaDeCompras = new ArrayList<>();
+
+    //Chamar metodo compra do Product na classe acima.
+    public void AddShopItens(Product p, int quant) {
         boolean jaComp = false;
-        for(ShoppingItem si: cestaDeCompras){
-            if(si.equals((Object)p)){
+        for (ShoppingItem si : cestaDeCompras) {
+            if (si.equals((Object) p)) {
                 int q = si.getQuant();
-                si.setQuant(quant+q);
+                si.setQuant(quant + q);
                 jaComp = true;
                 break;
             }
         }
-        if(!jaComp){
+        if (!jaComp) {
             cestaDeCompras.add(new ShoppingItem(p, quant));
         }
-        
+
     }
-    
-    
-    public String MostrarShopItens(){
+
+    public String MostrarShopItens() {
         String ShopItens = "";
-        for(ShoppingItem si : cestaDeCompras){
+        for (ShoppingItem si : cestaDeCompras) {
             ShopItens += si.toString();
             ShopItens += "\n";
         }
         return ShopItens;
     }
-    
-    public void apagarShopItens(Product p){
-        for(ShoppingItem si: cestaDeCompras){
-            if(si.equals((Object)p)){
+
+    public void apagarShopItens(Product p) {
+        for (ShoppingItem si : cestaDeCompras) {
+            if (si.equals((Object) p)) {
                 int quant = p.getQuantEmEstok();
-                p.setQuantEmEstok( quant + si.getQuant());
+                p.setQuantEmEstok(quant + si.getQuant());
                 cestaDeCompras.remove(si);
             }
         }
     }
-    
-    public void ModificarQuant(Product p, int quant){
-        if(quant >= 0){
-            for(ShoppingItem si: cestaDeCompras){
-                if(si.equals((Object)p)){
-                    if(quant > 0){
+
+    public void ModificarQuant(Product p, int quant) {
+        if (quant >= 0) {
+            for (ShoppingItem si : cestaDeCompras) {
+                if (si.equals((Object) p)) {
+                    if (quant > 0) {
                         int siQuant = si.getQuant();
-                        p.setQuantEmEstok(p.getQuantEmEstok()+si.getQuant());
+                        p.setQuantEmEstok(p.getQuantEmEstok() + si.getQuant());
                         boolean flag;
                         flag = p.comprar(quant);
-                        if(flag){
+                        if (flag) {
                             si.setQuant(quant);
-                        }else{
-                            p.setQuantEmEstok(p.getQuantEmEstok()-siQuant);
+                        } else {
+                            p.setQuantEmEstok(p.getQuantEmEstok() - siQuant);
                             si.setQuant(siQuant);
                         }
-                        
-                    }else if(quant == 0){
+                    } else if (quant == 0) {
                         this.apagarShopItens(p);
                     }
                 }
             }
         }
     }
-    
-    
 
 }
